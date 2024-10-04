22bbd78de28b22c515fec153284d0f5f50af43af
Willian José Henkel de Deus <willian.deus@hitechnologies.com.br>
2024-10-04 17:58:28 -0300

    added form

--- src/pages/CreateNewChartPage.vue
+++ src/pages/CreateNewChartPage.vue
@@ -12,13 +12,48 @@
 
     <q-card flat class="q-mt-xl">
       <q-card-section>
-        <q-input v-model="title" outlined label="Título do gráfico" />
+        <div class="text-h6 inter-bold">Título de gráfico</div>
+        <q-input v-model="title" outlined label="Novo gráfico" />
+      </q-card-section>
+    </q-card>
+
+    <q-card flat class="q-mt-md">
+      <q-card-section>
+        <div class="text-h6 inter-bold">Tipo de gráfico</div>
+        <div class="q-gutter-sm q-mb-sm">
+          <q-radio
+            v-model="type"
+            checked-icon="task_alt"
+            unchecked-icon="panorama_fish_eye"
+            val="setor"
+            label="Setores"
+          >
+          </q-radio>
+          <q-radio
+            v-model="type"
+            checked-icon="task_alt"
+            unchecked-icon="panorama_fish_eye"
+            val="bar"
+            label="Barra"
+          />
+        </div>
+        <div
+          style="background-color: #ededeb; width: 100px; border-radius: 8px"
+          class="flex justify-center"
+        >
+          <q-icon
+            :name="type === 'bar' ? 'equalizer' : 'pie_chart'"
+            size="5rem"
+            style="opacity: 0.8"
+          />
+        </div>
       </q-card-section>
     </q-card>
 
     <q-card flat class="q-mt-xl">
       <q-card-section>
-        <q-input v-model="title" outlined label="Título do gráfico" />
+        <div class="text-h6 inter-bold">Título de gráfico</div>
+        <q-input v-model="title" outlined label="Novo gráfico" />
       </q-card-section>
     </q-card>
   </q-page>
@@ -28,4 +63,5 @@
 import { ref } from 'vue';
 
 const title = ref('');
+const type = ref('');
 </script>
