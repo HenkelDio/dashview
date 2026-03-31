1a618823e106939156e81ffc1499311c0729acca
Henkel <willianhenkel@gmail.com>
2026-03-31 20:58:18 -0300

    1.7.0

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -70,7 +70,7 @@
           </div>
         </q-list>
         <div class="text-center text-caption inter-medium absolute-bottom">
-          Versão 1.6.9
+          Versão 1.7.0
         </div>
       </div>
     </q-drawer>
